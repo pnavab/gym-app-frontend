5254800cec61922c63aad89c37626b20ff9e7a1a
pnavab <114110926+pnavab@users.noreply.github.com>
2023-12-04 22:26:58 -0800

    Add fetching from api

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    async function getMessage() {
+      try {
+        const response = await fetch("https://gym-app-backend-pnavab.vercel.app/");
+        const data = await response.json();
+        setMessage(data.message);
+      } catch (error) {
+        console.error("error fetching message", error);
+      }
+    }
+
+    getMessage();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{message}</Text>
       <StatusBar style="auto" />
     </View>
   );
